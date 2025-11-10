5bd7a0c76613f832b52c1a0b54718a1ada24068f
Strange Knoll <StrangeKnoll@gmail.com>
2025-11-10 01:04:07 -0500

    Batch Export Improvements

--- 3x Addons/addons/TilesExporter/tile_export_plugin.gd
+++ 3x Addons/addons/TilesExporter/tile_export_plugin.gd
@@ -3,8 +3,8 @@ extends EditorPlugin
 
 const TileSetExporterInspectorPlugin = preload("tileset/tileset_inspector.gd")
 const TileMapExporterInspectorPlugin = preload("tilemap/tilemap_inspector.gd")
-const batch_converter_dialogue = preload("batch_convert/exporter_ui.tscn")
-const batch_converter_dialogue_script = preload("batch_convert/exporter_ui.gd")
+const batch_converter_dialogue = preload("batch_export/exporter_ui.tscn")
+const batch_converter_dialogue_script = preload("batch_export/exporter_ui.gd")
 
 const TOOL_MENU_LABEL = "TileMap/TileSet Batch Convert"
 
@@ -22,6 +22,7 @@ func _enter_tree():
 func start_batch_convert(_nothing_godot_is_silly): #Needed to make the tool menu work :/
 	var dialogue_base_node = batch_converter_dialogue.instance()
 	dialogue_base_node.set_script(batch_converter_dialogue_script)
+	dialogue_base_node.set_plugin(self)
 	
 	var base_control = get_editor_interface().get_base_control()
 	base_control.add_child(dialogue_base_node)
